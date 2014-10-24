7af1e2ae886d6ffadab818e05b79bcb80929f4df
Chris Holdgraf <choldgraf@berkeley.edu>
2014-10-23 17:12:30 -0700

    Added code to do some simple plotting with Bokeh that updates every time you send data to the server

--- web/tornado_setup.py
+++ web/tornado_setup.py
@@ -3,22 +3,33 @@ import tornado
 import requests
 import json
 import pandas as pd
+import time
+import numpy as np
 from eeg_viz import algorithms as api
 from IPython import embed
+from bokeh.plotting import output_server, line, figure, curplot, show, cursession
+from bokeh.objects import Glyph
 
 
 class MainHandler(web.RequestHandler):
     def handle_data(self, Fs=512, band='alpha'):
         df = api.read_dump(self.data_json, quality_cutoff=5)
         df = api.unravel_raw_values(df)
-        print type(band)
         self.data = df.groupby(level='id').apply(api.extract_band, Fs, band, kind='power')
+        y_disp = self.ds.data['y']
+        y_disp = np.roll(y_disp, 1)
+        y_disp[-1] = self.data.mean()
+        self.ds.data["y"] = y_disp
+        cursession().store_objects(self.ds)
+        time.sleep(.10)
         print self.data
 
     def get(self):
         self.write("Hello, world")
 
     def post(self):
+        renderer = [r for r in curplot().renderers if isinstance(r, Glyph)][0]
+        self.ds = renderer.data_source
         self.data_json = self.get_argument('data')
         try:
             band = self.get_argument('band')
@@ -35,6 +46,8 @@ class MainHandler(web.RequestHandler):
         if isinstance(band, unicode):
             band = str(band)
         self.handle_data(band=band, Fs=Fs)
+        self.write(self.data.to_json())
+        self.flush()
 
 
 def main(port=8989):
@@ -47,4 +60,11 @@ def main(port=8989):
 # In[4]:
 
 if __name__ == "__main__":
+    # Note - run python bokeh-server first
+    output_server('Server plotting')
+    last_ten_y = np.zeros(10)
+    last_ten_x = np.arange(10)
+    figure(x_range=[0, 10], tools="pan,wheel_zoom,box_zoom,reset,previewsave")
+    line(last_ten_x, last_ten_y)
+    show()
     main()
